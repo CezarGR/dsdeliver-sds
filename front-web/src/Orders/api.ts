cc58076ad60ed118297f3134330e02c6c018bb2e
CezarGR <cgabriel.lourenzo@gmail.com>
2021-01-07 23:47:03 -0300

    Correções

--- front-web/src/Orders/api.ts
+++ front-web/src/Orders/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URI = 'localhost:8080';
+const API_URI = 'http://localhost:8080';
 
 export function fetchProducts(){
     return axios(`${API_URI}/products`)

aa2b03f98c0d548103dbf5c468b01fb661bc6758
CezarGR <cgabriel.lourenzo@gmail.com>
2021-01-09 10:46:50 -0300

    Search order location

--- front-web/src/Orders/api.ts
+++ front-web/src/Orders/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API_URI = 'https://cesar-dsdeliry.herokuapp.com';
-
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 export function fetchProducts(){
     return axios(`${API_URI}/products`)
+}
+
+export function fetchLocalMapBox(local: string){
+    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
 }
